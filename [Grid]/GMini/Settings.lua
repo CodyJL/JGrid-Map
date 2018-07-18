2d8fa572445d9170afb615fbabe4ba64e4904fef
CodyJ <codylewiz@gmail.com>
2018-07-18 10:55:11 -0400

    Inital Commit

---
+++ [Grid]/GMini/Settings.lua
@@ -0,0 +1,6 @@
+radarSettings = {
+	['mapTexture'] = 'files/images/map.png', -- Don't change
+	['mapTextureSize'] = 3072, -- Don't change
+	['mapWaterColor'] = {102, 135, 168}, -- Adjusted for this map
+	['alpha'] = 200, -- Can change
+};
